44d1db8d18a3ae34f21ce99d4929362aa2cbfaba
kazeusagi <toshiki1098@gmail.com>
2025-03-31 01:41:18 +0000

    add: 各IaCの確認中

---
+++ src/awsCdk/3.5.1/test/3.5.1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 351 from '../lib/3.5.1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/3.5.1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 351.351Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
