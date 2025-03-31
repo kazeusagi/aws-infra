423518274f9917e53e0d764eb092b5e60145a64e
kazeusagi <toshiki1098@gmail.com>
2025-03-31 06:30:26 +0000

    練習中

--- src/awsCdk/3/lib/sqs-stack.ts
+++ src/awsCdk/3/lib/sqs-stack.ts
@@ -2,15 +2,17 @@ import * as cdk from "aws-cdk-lib";
 import type { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 
-export class MyStack extends cdk.Stack {
+export class SqsStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		// The code that defines your stack goes here
 
 		// example resource
-		const queue = new sqs.Queue(this, "351Queue", {
-			visibilityTimeout: cdk.Duration.seconds(300),
+		const queue = new sqs.Queue(this, "MyQueue", {
+			queueName: "test-queue-cdk",
+			maxMessageSizeBytes: 2048,
 		});
+		cdk.Tags.of(queue).add("Name", "test-queue-cdk");
 	}
 }
